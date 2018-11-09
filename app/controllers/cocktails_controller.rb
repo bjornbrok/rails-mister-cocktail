7e48d5bfade3fa263c7f9b80e02efe700586f13a
Bjorn Brok <bjbr04@gmail.com>
2018-11-09 16:52:42 +0100

    finished mister-cocktail ugly vers

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,9 +13,16 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    cocktail = Cocktail.new(cocktail_params)
-    cocktail.save
-    redirect_to cocktails_path
+    @cocktail = Cocktail.new(cocktails_params)
+    # @cocktail.save
+    #redirect_to cocktail_path(@cocktail)
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+
   end
 
   private
